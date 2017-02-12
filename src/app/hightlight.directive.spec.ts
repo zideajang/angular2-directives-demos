e52be465bcd01d8f7e2062b0e76cbc6fe61865fa
zideajang <zidea2015@163.com>
2017-02-12 11:59:02 +0800

    ngStyle and ngClass demos

---
+++ src/app/hightlight.directive.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { HightlightDirective } from './hightlight.directive';
+
+describe('HightlightDirective', () => {
+  it('should create an instance', () => {
+    const directive = new HightlightDirective();
+    expect(directive).toBeTruthy();
+  });
+});
